257d659451fe795c8b4416e8ed97d16a4642aa05
Hossain Ahmed <ahmed-hossain2@live.com>
2018-06-04 03:21:41 +0000

    Class work 4

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 /* Task 1 */
-
+    
 
 /* Task 2 using plain JavaScript */
 
 
 
 /* Task 4 using jQuery */
+    //4A
+    $("#btn").click(function(event) {
+        var para = $('<p>').text("Hello World");
+        $('#task4a').append(para);
+    })
+    
+    
+    // 4C
+    $("#btn-add").click(function(event) {
+        var numOne = $("#numOne").val();
+        var numTwo = $("#numTwo").val();
+        
+        var result = Number(numOne) + Number(numTwo);
+        
+        $("#result").append(result);
+    })
